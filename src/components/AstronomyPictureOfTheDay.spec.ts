e670b13652538ccfcc3a0ab0ff67c0f03cdd0051
ursteiner <christoph.mueller.zw@googlemail.com>
2025-12-23 14:26:04 +0100

    define type for API response

--- src/components/AstronomyPictureOfTheDay.spec.ts
+++ src/components/AstronomyPictureOfTheDay.spec.ts
@@ -1,15 +1,16 @@
 import {mount} from '@vue/test-utils'
 import {describe, expect, it} from 'vitest'
-import AstronomyPictureOfTheDay from './AstronomyPictureOfTheDay.vue'
+import AstronomyPictureOfTheDayComponent from './AstronomyPictureOfTheDay.vue'
 import ResizeObserver from './../mocks/ResizeObserver'
 import vuetify from './../plugins/vuetify'
+import type {AstronomyPictureOfTheDay} from "@/types/AstronomyPictureOfTheDay.ts";
 
 describe('AstronomyPictureOfTheDay', () => {
   it('mount component', async () => {
     window.ResizeObserver = ResizeObserver
-    expect(AstronomyPictureOfTheDay).toBeTruthy()
+    expect(AstronomyPictureOfTheDayComponent).toBeTruthy()
 
-    const wrapper = mount(AstronomyPictureOfTheDay, {
+    const wrapper = mount(AstronomyPictureOfTheDayComponent, {
       global: {
         plugins: [vuetify],
       },
@@ -19,7 +20,8 @@ describe('AstronomyPictureOfTheDay', () => {
           date: '2025-06-29',
           explanation: 'Test explanation',
           copyright: 'copyright',
-        },
+        } as AstronomyPictureOfTheDay,
+        loading: false,
         error: '',
       },
     })
--- src/components/AstronomyPictureOfTheDay.vue
+++ src/components/AstronomyPictureOfTheDay.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-
 import type {AstronomyPictureOfTheDay} from "@/types/AstronomyPictureOfTheDay.ts";
 
 const props = defineProps<{
